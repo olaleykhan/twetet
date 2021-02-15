982c17c31df08c145eb496001982314456e5b36b
olaleykhan <realjoy9@gmail.com>
2021-02-15 17:43:31 +0100

    first vue app. first commit

---
+++ twetet/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <user/>
+    
+  </div>
+</template>
+
+<script>
+import userProfile from "./components/userProfile"
+
+export default {
+  name: 'App',
+  components:{
+    user:userProfile
+  }
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ twetet/src/components/userProfile.vue
@@ -0,0 +1,65 @@
+<template>
+    <div>
+        <h1 > {{fullName}} aka @{{user.userName}} is logged in</h1>
+    <p> Followers: {{followers}} </p>
+    <div class="user-badge" v-if="user.isAdmin"> Admin</div>
+    <button @click="followUser" > Follow me</button>
+    <div class="tweets" :key="i" v-for="(tweet,i) in user.tweets">
+        {{tweet.content}}
+
+    </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+     data(){
+    return{
+      followers: 0,
+      user:{
+        id: 1,
+        userName: 'alaf',
+        firstName: 'Abdullah',
+        lastName: 'olalekan',
+        email: 'bashxyz@gmail.com',
+        isAdmin: true,
+        tweets:[
+            {id:1, content:"Follow back everyone. I am here to stay"},
+            {id:2, content:"come to my home on monday."},
+            {id:3, content:"I'm here bitches"}
+        ]
+      }
+    }
+
+  },
+  computed: {
+    fullName(){
+      return `${this.user.firstName} ${this.user.lastName} `
+    }
+  },
+  methods: {
+    followUser(){
+      this.followers++;
+    }
+  },
+  mounted(){
+      this.followUser();
+  }
+ 
+}
+</script>
+
+<style scoped>
+.user-badge{
+    background-color: rebeccapurple;
+    font-weight: 700;
+    width: 100px;
+    border-radius: 4px;
+    text-align: center;
+    margin: 5px auto;
+    padding: 5px;
+    list-style-type:armenian;
+    color: white;
+}
+</style>
